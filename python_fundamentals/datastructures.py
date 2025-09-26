1e2fedbd9baf65728f76b65dd6cbf3648139d86b
microprixs <arpit.mishra@microprixs.in>
2025-09-26 11:30:14 +0530

    Solved problems on functions in it

--- python_fundamentals/datastructures.py
+++ python_fundamentals/datastructures.py
@@ -42,3 +42,88 @@ friends_set = set(["Arpit","Rajesh"])
 # Tuples are defined using parentheses ()
 
 planets = ("Mercury","Venus","Earth","Mars","Jupiter","Saturn","Uranus","Neptune")
+
+
+# Programs
+
+def get_largest_number(*args):
+    largest_number = None
+
+    for number in args:
+        if largest_number is None or largest_number < number:
+            largest_number = number
+
+    return largest_number
+
+
+# largest_number = get_largest_number(78,14,56,98,78,45,65,132,795,-78,20,15)
+# largest_number = get_largest_number(-1,-2,-3,-785)
+# print(f"Largest Number Is: {largest_number}")
+
+
+def find_sum(*args):
+    total = 0
+    for number in args:
+        total += number
+
+    return total
+
+# total = find_sum(78,25,12,34,56,78,954)
+# print(f"Total Sum Result Is: {total}")
+
+
+# def reverse_string(str):
+#     return str[::-1]
+
+# input_str = input("Enter String: ")
+# print(f"Original String IS: {input_str}")
+# output_str = reverse_string(input_str)
+# print(f"Reversed String Is: {output_str}")
+
+
+def find_factorial(number):
+    fact = 1
+    for num in range(1,number+1):
+        fact = fact * num
+
+    return fact
+
+
+# number = int(input("Enter Number: "))
+# factorial = find_factorial(number)
+# print(f"Factorial of {number} is: {factorial}")
+
+# def letter_case_count(str) -> list[int,int]:
+#     upper_count = 0
+#     lower_count = 0
+
+#     for ch in str:
+#         if ch.isupper():
+#             upper_count += 1
+
+#         elif ch.islower():
+#             lower_count += 1
+
+#         else:
+#             pass
+
+#     return [upper_count,lower_count]
+
+# input_string = input("Enter String: ")
+# upper_count,lower_count = letter_case_count(input_string)
+# print(f"Upper Case Letters: {upper_count}")
+# print(f"Lower Case Letters: {lower_count}")
+
+
+# def is_pallindrome(str) -> bool:
+#     reversed_string = str[::-1]
+#     if str == reversed_string:
+#         return True
+#     return False
+
+# input_string = input("Enter String: ")
+# result = is_pallindrome(input_string)
+# print(f"String {input_string} is {'Is A Palindrome String' if result else 'Not A Palindrome String '}")
+
+
+
