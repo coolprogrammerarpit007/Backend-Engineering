0e82aaca18aae225e38322a29cb4e840e707172a
microprixs <arpit.mishra@microprixs.in>
2025-10-02 12:49:27 +0530

    Asynchronous Python real world examples

--- Python-Asyncio/example_7.py
+++ Python-Asyncio/example_7.py
@@ -9,24 +9,25 @@ async def fetch_data(param):
 
 async def main():
     # Create Tasks Manually
-    task1 = asyncio.create_task(fetch_data(1))
-    task2 = asyncio.create_task(fetch_data(2))
-    result1 = await task1
-    result2 = await task2
-    print(f"Task 1 and 2 awaited results: {[result1, result2]}")
+    # task1 = asyncio.create_task(fetch_data(1))
+    # task2 = asyncio.create_task(fetch_data(2))
+    # result1 = await task1
+    # result2 = await task2
+    # print(f"Task 1 and 2 awaited results: {[result1, result2]}")
 
     # Gather Coroutines
-    coroutines = [fetch_data(i) for i in range(1, 3)]
-    results = await asyncio.gather(*coroutines, return_exceptions=True)
-    print(f"Coroutine Results: {results}")
+    # coroutines = [fetch_data(i) for i in range(1, 3)]
+    # results = await asyncio.gather(*coroutines, return_exceptions=True)
+    # print(f"Coroutine Results: {results}")
 
     # Gather Tasks
-    tasks = [asyncio.create_task(fetch_data(i)) for i in range(1, 3)]
-    results = await asyncio.gather(*tasks)
-    print(f"Task Results: {results}")
+    # tasks = [asyncio.create_task(fetch_data(i)) for i in range(1, 3)]
+    # results = await asyncio.gather(*tasks)
+    # print(f"Task Results: {results}")
 
     # Task Group
     async with asyncio.TaskGroup() as tg:
+        # once we enter the context-manager thus when the main coroutine got suspended
         results = [tg.create_task(fetch_data(i)) for i in range(1, 3)]
         # All tasks are awaited when the context manager exits.
     print(f"Task Group Results: {[result.result() for result in results]}")
